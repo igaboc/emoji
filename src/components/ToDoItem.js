d83e6b8cdf25ea34a8754b06b384f09fefe80312
igaboc <isabellegaboc@hotmail.com>
2017-11-28 15:03:10 +1100

    Refactor Todo Item checkbox to instead use emoji inside a button

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -5,22 +5,32 @@ function ToDoItem({
     completed = false, // if not passed, default to 'false'
     onToggleCompleted
 }) {
+    let checkbutton = ''
+    if (completed === false) {
+        checkbutton = '❌'
+    }
+    else {
+        checkbutton = '✅'
+    }
     return (
-        <label> 
-            <input 
-                type='checkbox' 
-                checked={completed} 
-                onChange={
-                    (event) => { // Event listener
-                        console.log('Clicked checkbox', description)
-                        // When user clicks checkbox
-                        // Notify the powers above that the concept of completed has changed
-                        onToggleCompleted()
+        <button>
+            <label> 
+                <input 
+                    type='checkbox' 
+                    checked={completed} 
+                    onChange={
+                        (event) => { // Event listener
+                            console.log('Clicked checkbox', description)
+                            // When user clicks checkbox
+                            // Notify the powers above that the concept of completed has changed
+                            onToggleCompleted()
+                        }
                     }
-                }
-            />
-            { description }
-        </label>
+                />
+                {checkbutton}
+                { description }
+            </label>
+        </button>
     )
 }
 
