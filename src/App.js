4016e2837a151c2d5974203c00cc380b31807b8d
igaboc <isabellegaboc@hotmail.com>
2017-11-28 12:13:27 +1100

    Refactor App.js to refactor Component to remove hard coded items and use JSON

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import ToDoItem from './components/ToDoItem'
 
+// Data (model)
+const items = [
+  { description: 'First', completed: true },
+  { description: 'Second', completed: true },
+  { description: 'Third', completed: false }
+]
+
+// UI (view)
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ToDoItem description='First' completed />
-        <ToDoItem description='Second' completed />
-        <ToDoItem description='Third' />
+      {
+        items.map((item, index) => (
+          <ToDoItem 
+            key={ index }
+            description={ item.description }
+            completed={ item.completed }
+          />
+        ))
+      }
       </div>
     );
   }
